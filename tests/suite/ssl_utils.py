bbc72f103dd10bcf112adfaff4756301f438a5d1
Venktesh Patel <ve.patel@f5.com>
2021-04-14 15:37:06 +0100

    modify get_cert

--- tests/suite/ssl_utils.py
+++ tests/suite/ssl_utils.py
@@ -16,18 +16,23 @@ def get_certificate(ip_address, host, port, timeout=10) -> str:
     :param timeout:
     :return: str
     """
+    print(ssl.HAS_SNI)
+    print(ssl.OPENSSL_VERSION)
     context = ssl.create_default_context()
     context.check_hostname = False
     context.verify_mode = ssl.CERT_NONE
-    conn = socket.create_connection((ip_address, port))
+    # conn = socket.create_connection((ip_address, port))
     server_hostname = host if ssl.HAS_SNI else None
-    sock = context.wrap_socket(conn, server_hostname=server_hostname)
-    sock.settimeout(timeout)
-    try:
-        der_cert = sock.getpeercert(True)
-    finally:
-        sock.close()
-    return ssl.DER_cert_to_PEM_cert(der_cert)
+    with socket.create_connection((ip_address, port)) as sock:
+        with context.wrap_socket(sock, server_hostname=server_hostname) as ssock:
+            print(ssock.version())
+    # sock = context.wrap_socket(conn, server_hostname=server_hostname)
+            ssock.settimeout(timeout)
+            try:
+                der_cert = ssock.getpeercert(True)
+            finally:
+                ssock.close()
+            return ssl.DER_cert_to_PEM_cert(der_cert)
 
 
 def get_server_certificate_subject(ip_address, host, port=443) -> dict:
