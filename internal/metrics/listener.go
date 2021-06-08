ddd22fe71aac3c150f8d45104f9d9cb18e880f6b
Sean O'Neill <s.oneill@f5.com>
2021-06-08 17:16:17 +0100

    Add HTTPS option to prometheus endpoint

--- internal/metrics/listener.go
+++ internal/metrics/listener.go
@@ -2,8 +2,12 @@ package metrics
 
 import (
 	"fmt"
+	"io/ioutil"
+	api_v1 "k8s.io/api/core/v1"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 
 	"github.com/golang/glog"
 	prometheusClient "github.com/nginxinc/nginx-prometheus-exporter/client"
@@ -21,18 +25,18 @@ func NewNginxMetricsClient(httpClient *http.Client) (*prometheusClient.NginxClie
 }
 
 // RunPrometheusListenerForNginx runs an http server to expose Prometheus metrics for NGINX
-func RunPrometheusListenerForNginx(port int, client *prometheusClient.NginxClient, registry *prometheus.Registry, constLabels map[string]string) {
+func RunPrometheusListenerForNginx(port int, client *prometheusClient.NginxClient, registry *prometheus.Registry, constLabels map[string]string, prometheusSecret *api_v1.Secret) {
 	registry.MustRegister(nginxCollector.NewNginxCollector(client, "nginx_ingress_nginx", constLabels))
-	runServer(strconv.Itoa(port), registry)
+	runServer(strconv.Itoa(port), registry, prometheusSecret)
 }
 
 // RunPrometheusListenerForNginxPlus runs an http server to expose Prometheus metrics for NGINX Plus
-func RunPrometheusListenerForNginxPlus(port int, nginxPlusCollector prometheus.Collector, registry *prometheus.Registry) {
+func RunPrometheusListenerForNginxPlus(port int, nginxPlusCollector prometheus.Collector, registry *prometheus.Registry, prometheusSecret *api_v1.Secret) {
 	registry.MustRegister(nginxPlusCollector)
-	runServer(strconv.Itoa(port), registry)
+	runServer(strconv.Itoa(port), registry, prometheusSecret)
 }
 
-func runServer(port string, registry prometheus.Gatherer) {
+func runServer(port string, registry prometheus.Gatherer, prometheusSecret *api_v1.Secret) {
 	http.Handle(metricsEndpoint, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -49,5 +53,47 @@ func runServer(port string, registry prometheus.Gatherer) {
 	})
 	address := fmt.Sprintf(":%v", port)
 	glog.Infof("Starting Prometheus listener on: %v%v", address, metricsEndpoint)
-	glog.Fatal("Error in Prometheus listener server: ", http.ListenAndServe(address, nil))
+	if prometheusSecret == nil {
+		glog.Fatal("Error in Prometheus listener server: ", http.ListenAndServe(address, nil))
+	} else {
+		// Unfortunately, http.ListenAndServeTLS() takes a filename instead of cert/key data, so we
+		// Write the cert and key to a temporary file. We create a unique file name to prevent collisions.
+		certFileName := fmt.Sprintf("%v-%s", time.Now().Unix(), "nginx-prometheus.cert")
+		keyFileName := fmt.Sprintf("%v-%s", time.Now().Unix(), "nginx-prometheus.key")
+		certFile, err := writeTempFile(prometheusSecret.Data[api_v1.TLSCertKey], certFileName)
+		if err != nil {
+			glog.Errorf("failed to create cert file for prometheus: %w", err)
+		}
+		defer removeTemporaryFile(certFile)
+
+		keyFile, err := writeTempFile(prometheusSecret.Data[api_v1.TLSPrivateKeyKey], keyFileName)
+		if err != nil {
+			glog.Errorf("failed to create key file for prometheus: %w", err)
+		}
+		defer removeTemporaryFile(keyFile)
+
+		glog.Fatal("Error in Prometheus listener server: ", http.ListenAndServeTLS(address, certFile.Name(), keyFile.Name(), nil))
+	}
+}
+
+func writeTempFile(data []byte, name string) (*os.File, error) {
+	f, err := ioutil.TempFile("", name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp file: %w", err)
+	}
+	fmt.Println("Temp file name:", f.Name())
+
+	_, err = f.Write(data)
+	if err != nil {
+		return f, fmt.Errorf("failed to write to temp file: %w", err)
+	}
+
+	return f, nil
+}
+
+func removeTemporaryFile(file *os.File) {
+	err := os.Remove(file.Name())
+	if err != nil && !os.IsNotExist(err) {
+		glog.Warningf("failed to remove temp cert file for prometheus: %w", err)
+	}
 }
